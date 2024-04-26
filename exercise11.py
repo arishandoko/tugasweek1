bfd46d4bbb13b6c649b93b35d0850b87b821ad94
aris <arishandoko33@gmail.com>
2024-04-26 20:00:53 +0700

    lanjut exercise 11

---
+++ exercise11.py
@@ -0,0 +1,8 @@
+# Exercise 11
+# Write a function definition for a function named add_one that takes in a number and returns that number plus one.
+def add_one(number):
+  return number + 1
+
+print(add_one(2))
+print(add_one(0)) 
+ 
